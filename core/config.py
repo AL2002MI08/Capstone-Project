452d5b89f3da1a2942b18ffe67ffcb3fbeb0194f
AL2002MI08 <alexandramizero@gmail.com>
2026-05-17 18:54:18 +0200

    chore: configure settings to load from .env file using Pydantic SettingsConfigDict

--- core/config.py
+++ core/config.py
@@ -1,9 +1,11 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = 'sqlite:///./vacation.db'
-    SECRET_KEY: str = 'secret_key'
-    ALGORITHM: str = 'HS256'
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings()
