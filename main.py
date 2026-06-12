f2e1b58ed43380e80ac0d63358fc3a637e2be406
AL2002MI08 <alexandramizero@gmail.com>
2026-06-12 19:23:20 +0200

    chore: add ANTHROPIC_API_KEY to config and finalize initial database schema migration

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from core.db import init_db
 from controllers import user_routes, auth, trip, itinerary
 from models import User, Trip, Itinerary
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    init_db()
     yield
 
 app = FastAPI(lifespan=lifespan)
