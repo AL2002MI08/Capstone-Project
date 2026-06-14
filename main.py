6619c9dde411243b84a94784959d8559f7a3e356
AL2002MI08 <alexandramizero@gmail.com>
2026-06-14 14:57:12 +0200

    fix: remove manual db init to enforce alembic migrations

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 from controllers import user_routes, auth, trip, itinerary
-from models import User, Trip, Itinerary
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(auth.router)
 app.include_router(user_routes.router, prefix="/users")
 app.include_router(trip.router)
