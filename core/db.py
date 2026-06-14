d4a8590d849b5c6bb407734e8faf95e35721308b
AL2002MI08 <alexandramizero@gmail.com>
2026-06-14 14:58:26 +0200

    refactor: remove manual db init

--- core/db.py
+++ core/db.py
@@ -6,5 +6,3 @@ engine = create_engine(settings.DATABASE_URL, echo=True)
 def get_session():
     with Session(engine) as session:
         yield session
-def init_db():
-    SQLModel.metadata.create_all(engine)
