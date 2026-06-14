04a5b76d616a44c316955e245a8217be1788ab01
AL2002MI08 <alexandramizero@gmail.com>
2026-06-14 15:04:56 +0200

    feat: integrate llm to automate itinerary generation from trip details

--- services/itinerary_service.py
+++ services/itinerary_service.py
@@ -4,20 +4,49 @@ from schemas.itinerary import ItineraryResponse
 from models.trip import Trip
 from models.itinerary import Itinerary
 from fastapi import HTTPException
-from schemas.itinerary import ItineraryCreate
 from sqlmodel import Session
+from core.config import settings
+from anthropic import Anthropic
+import json
+from core.prompts import system_prompt, prompt
 
+client = Anthropic(api_key=settings.ANTHROPIC_API_KEY)
 
+def add_user_message(messages, text):
+    user_message = {"role": "user", "content": text}
+    messages.append(user_message)
+    return messages
 
+def add_assistant_message(messages, text):
+    messages.append({"role": "assistant", "content": text})
+    return messages
 
-def create_itinerary(session: Session, itinerary: ItineraryCreate):
-    trip = session.get(Trip, itinerary.trip_id)
+def generate_itinerary(session: Session, trip_id: int):
+    trip = session.get(Trip, trip_id)
     if not trip:
         raise HTTPException(status_code=404, detail="Trip not found")
+    itinerary_message = prompt.format(
+        destination=trip.destination,
+        days=trip.days,
+        budget=trip.budget,
+        travel_style=trip.trip_style
+    )
     
+    message = add_user_message([], itinerary_message)
+    prefilled_message = add_assistant_message(message, "[")
+    
+    output = client.messages.create(
+        model="claude-haiku-4-5-20251001",
+        max_tokens=2000,
+        messages=prefilled_message,
+        system=system_prompt,
+        temperature=0.6
+    )
+    raw = "[" + output.content[0].text
+    itinerary = json.loads(raw)
     db_itinerary = Itinerary(
-        trip_id=itinerary.trip_id,
-        days=[day.model_dump() for day in itinerary.days],
+        trip_id=trip.id,
+        days=itinerary,
     )
     session.add(db_itinerary)
     session.commit()
@@ -28,6 +57,7 @@ def create_itinerary(session: Session, itinerary: ItineraryCreate):
         days=[DayEntry(**day) for day in db_itinerary.days],
         message="Itinerary created successfully!"
     )
+    
 
 def get_itinerary(session: Session, trip_id: int):
     statement = select(Itinerary).where(Itinerary.trip_id == trip_id)
